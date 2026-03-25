c51b602a90b8138393f3f86917f5927b55781dfd
Zeyu Yang <zeyu.yang@tu-dortmund.de>
2026-03-25 15:18:03 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    application
+}
+
+group = "io.github.zeyuyang.sixcardgolf"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(group = "tools.aqua", name = "bgw-gui", version = "0.9")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    applicationDefaultJvmArgs = listOf(
+        "--add-opens", "java.desktop/sun.awt=ALL-UNNAMED",
+        "--add-opens", "java.desktop/java.awt.peer=ALL-UNNAMED",
+        "--add-opens", "java.desktop/sun.lwawt=ALL-UNNAMED",
+        "--add-opens", "java.desktop/sun.lwawt.macosx=ALL-UNNAMED"
+    )
+}
